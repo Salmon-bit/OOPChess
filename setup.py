2333df0c4bda862c1822ab53b8535f1cbe2f482b
Salmon228 <salmon228@inbox.ru>
2026-04-25 16:49:41 +0300

    Added License

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
   name='ultimate-chesslib',
-  version='0.0.2',
+  version='0.0.3',
   author='IgorGrebenchikov',
   author_email='grebenchikov_igor@mail.ru',
   description='This is ultimate library to create your own chess simulator!',
